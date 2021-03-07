0c696a7f3adc0ff37b84ce0542b310af29513405
Jeleo <jeleo1105sasuke@gmail.com>
2021-03-08 08:42:36 +0900

    拠点、地面の配置および味方ユニットハンドガンの動き

---
+++ Defense_Game/Assets/Scripts/ShootingV2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingV2 : MonoBehaviour
+{
+
+    // 弾のGameObject
+    public GameObject bullet;
+
+    // 弾丸発射点のGameObjectのTransfom
+    public Transform muzzle;
+
+    public float speed = 10000; // 弾丸の速度
+    public float timeOut = 20; // 時間を打つ間隔を指定
+    private float timeElapsed; //経過時間を格納
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= timeOut)
+        {
+            // 弾丸の複製(プレファブした弾GObjectを)
+            GameObject bullets = Instantiate(bullet) as GameObject;
+
+            // 変数：弾の力
+            Vector3 force;
+
+            // 弾丸のはじめ位置を調整
+            bullets.transform.position = muzzle.position;
+            // foceを計算する
+            force = this.gameObject.transform.forward * speed;
+
+            // Rigidbodyに力を加える
+            bullets.GetComponent<Rigidbody>().AddForce(force);
+
+            timeElapsed = 0.0f;
+
+        }
+
+
+    }
+
+}
